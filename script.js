8f01541284ad949553c64c179243e79814c873da
Oksana Budko <budkoksana@gmail.com>
2026-08-27 23:14:50 +0300

    feat: create square on the page and add moving event on arrows for square

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict'
 const body = document.querySelector('body');
 
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg, fontSize, position) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.position = position;
 
     this.creatNewElem = function () {
         let firstCharSelector = this.selector[0];
@@ -25,15 +26,34 @@ const DomElement = function (selector, height, width, bg, fontSize) {
         newElem.style.cssText = `width: ${this.width}; 
         height: ${this.height};
         background-color: ${this.bg};
-        font-size: ${this.fontSize}`
+        font-size: ${this.fontSize};
+        position: ${this.position}`
 
         body.append(newElem);
 
     }
 }
 
-const newElem = new DomElement('.classDiv', 200, 150, 'green', 20);
-const newElem1 = new DomElement('#classP', 100, 150, 'red', 20);
+const newElem = new DomElement('.square', '100px', '100px', 'green', 20, 'absolute');
 
-newElem1.creatNewElem();
-newElem.creatNewElem();
+
+document.addEventListener('DOMContentLoaded', newElem.creatNewElem.bind(newElem));
+document.addEventListener('keydown', (event) => {
+    let elem = document.querySelector('.square');
+    const currentX = elem.offsetLeft;
+    const currentY = elem.offsetTop;
+
+    if (event.key === 'ArrowRight') {
+        console.log('right');
+        elem.style.left = (currentX + 10) + 'px';
+    } else if (event.key === 'ArrowLeft') {
+        console.log('left');
+        elem.style.left = (currentX - 10) + 'px';
+    } else if (event.key === 'ArrowUp') {
+        console.log('up');
+        elem.style.top = (currentY - 10) + 'px';
+    } else if (event.key === 'ArrowDown') {
+        console.log('down');
+        elem.style.top = (currentY + 10) + 'px';
+    }
+})
