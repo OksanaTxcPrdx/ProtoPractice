89c2fb0d0c9db2446b0271a81911f203bcaad4e2
Oksana Budko <budkoksana@gmail.com>
2026-09-20 23:28:09 +0300

    feat: refactor structure of keeping data about dynamic fields, add func for renderinc dynamic fields depend on selected plant, add func for collect all data form inputs

--- lesson17_oop_entity_crud_localstorage/script.js
+++ lesson17_oop_entity_crud_localstorage/script.js
@@ -1,15 +1,16 @@
 const plantSelect = document.getElementById('plant-type-select');
-const plantNameInput = document.getElementById('plant-name-input');
-const plantAgeInput = document.getElementById('plant-age-input');
-const plantAreaInput = document.getElementById('plant-area-input');
-const plantIsToxicChb = document.getElementById('plant-is-toxic-checkbox');
+const plantForm = document.getElementById('plant-form');
+// const plantNameInput = document.getElementById('plant-name-input');
+// const plantAgeInput = document.getElementById('plant-age-input');
+// const plantAreaInput = document.getElementById('plant-area-input');
+// const plantIsToxicChb = document.getElementById('plant-is-toxic-checkbox');
 const dynamicFields = document.querySelector('.dynamic-fields');
 const saveBtn = document.getElementById('save-btn');
 const plantsTable = document.querySelector('table');
 
 class Plant {
 
-    constructor(name, age, area, isToxic) {
+    constructor({ name, age, area, isToxic }) {
         this._name = name;
         this._age = age;
         this._area = area;
@@ -50,8 +51,8 @@ class Plant {
 }
 
 class Fern extends Plant {
-    constructor(name, age, area, isToxic, fondsLength, habitat) {
-        super(name, age, area, isToxic);
+    constructor({ fondsLength, habitat, ...basePlantData }) {
+        super(basePlantData);
         this.fondsLength = fondsLength;
         this.habitat = habitat;
     }
@@ -60,11 +61,13 @@ class Fern extends Plant {
     }
 }
 
+
+
 class Spruse extends Plant {
-    constructor(name, age, area, isToxic, needlsLength, frostResistance) {
-        super(name, age, area, isToxic);
+    constructor({ needlsLength, droughtResistance, ...basePlantData }) {
+        super(basePlantData);
         this.needlsLength = needlsLength;
-        this.frostResistance = frostResistance;
+        this.droughtResistance = droughtResistance;
     }
 
     deleteFormStorage() {
@@ -72,59 +75,100 @@ class Spruse extends Plant {
     }
 }
 
-const fillDynamicFields = function (lbl, input) {
-    const formGroupWrapper = document.createElement('div');
-    formGroupWrapper.classList.add('form-group');
-    formGroupWrapper.append(lbl, input);
-
-    dynamicFields.append(formGroupWrapper);
+const PLANT_CONFIGS = {
+    fern: {
+        plantClass: Fern,
+        fields: [
+            {
+                name: 'fondsLength',
+                label: 'Длина вий(cm): ',
+                type: 'number',
+                required: true,
+            },
+            {
+                name: 'habitat',
+                label: 'Среда обитания: ',
+                type: 'text',
+                required: true,
+            },
+        ]
+    },
+    spruse: {
+        plantClass: Spruse,
+        fields: [
+            {
+                name: 'needlsLength',
+                label: 'Средняя длина иголок(cm): ',
+                type: 'number',
+                required: true,
+            },
+            {
+                name: 'droughtResistance',
+                label: 'Засухоустойчивость: ',
+                type: 'text',
+                required: true,
+            },
+        ]
+    }
 }
 
-const placeFernFields = function () {
+const renderDynamicFields = function () {
     dynamicFields.textContent = '';
-    const fernFondsLengthLbl = document.createElement('label');
-    fernFondsLengthLbl.textContent = 'Длина вий';
-    const fernHabitatLbl = document.createElement('label');
-    fernHabitatLbl.textContent = 'Среда обитания'
-    const fernFondsLengthInput = document.createElement('input');
-    fernFondsLengthInput.type = 'number';
-    const fernHabitatInput = document.createElement('input');
+    let selectedPlant = plantSelect.selectedOptions[0].value;
+    PLANT_CONFIGS[selectedPlant].fields.forEach(field => {
+        const lbl = document.createElement('label');
+        lbl.textContent = field.label;
+        const input = document.createElement('input');
+        input.type = field.type;
+        input.required = field.required;
+        input.name = field.name;
 
-    fillDynamicFields(fernFondsLengthLbl, fernFondsLengthInput);
-    fillDynamicFields(fernHabitatLbl, fernHabitatInput);
+        const formGroupWrapper = document.createElement('div');
+        formGroupWrapper.classList.add('form-group');
+        formGroupWrapper.append(lbl, input);
+
+        dynamicFields.append(formGroupWrapper);
+    });
 }
 
-const placeSpruseFields = function () {
-    dynamicFields.textContent = '';
-    const needlsLengthLbl = document.createElement('label');
-    needlsLengthLbl.textContent = 'Средняя длина иголок';
-    const frostResistanceLbl = document.createElement('label');
-    frostResistanceLbl.textContent = 'Перенесение морозов';
-    const needlsLengthInput = document.createElement('input');
-    needlsLengthInput.type = 'number';
-    const frostResistanceInput = document.createElement('input');
 
-    fillDynamicFields(needlsLengthLbl, needlsLengthInput);
-    fillDynamicFields(frostResistanceLbl, frostResistanceInput);
+const collectPlantData = function () {
+    let colectedPlantData = {};
+    let selectedPlant = plantSelect.selectedOptions[0].value;
+    let PlantClass = PLANT_CONFIGS[selectedPlant].plantClass;
+
+    let inputsList = plantForm.querySelectorAll('input');
+    inputsList.forEach(input => {
+        if (input.type === 'checkbox') {
+            colectedPlantData[input.name] = input.checked;
+        } else if (input.type === 'number') {
+            colectedPlantData[input.name] = +input.value;
+        } else {
+            colectedPlantData[input.name] = input.value;
+        }
+    })
+    console.log(colectedPlantData);
+    let newPlant = new PlantClass(colectedPlantData);
+    console.log(newPlant);
 }
 
 const onload = function () {
-    document.addEventListener('DOMContentLoaded', placeFernFields());
-    plantSelect.addEventListener('change', () => {
-        let selectedPlant = plantSelect.selectedOptions[0].value;
-        console.log(selectedPlant);
-        if (selectedPlant === 'fern') {
-            placeFernFields();
-        } else if (selectedPlant === 'spruse') {
-            placeSpruseFields();
-        }
+    document.addEventListener('DOMContentLoaded', renderDynamicFields);
+
+    plantSelect.addEventListener('change', renderDynamicFields);
+    //plantSelect.addEventListener('change', collectPlantData);
+    saveBtn.addEventListener('click', () => {
+        event.preventDefault();
+        collectPlantData();
     })
 }
 
-console.dir(plantSelect);
 
-const plant = new Plant();
-const fern = new Fern();
-const spruse = new Spruse();
+
+// console.dir(plantSelect);
+
+// const plant = new Plant();
+// const fern = new Fern();
+// const spruse = new Spruse();
 
 onload();
